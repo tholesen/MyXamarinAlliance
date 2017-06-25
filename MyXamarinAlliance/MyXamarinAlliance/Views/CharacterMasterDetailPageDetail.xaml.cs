d663563d471e74070340786301a3c31d160e6ada
Thorbjorn Olesen <thorbjorn.olesen@aveva.com>
2017-06-25 14:42:01 +0200

    challenge 1+2

---
+++ MyXamarinAlliance/MyXamarinAlliance/Views/CharacterMasterDetailPageDetail.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinAllianceApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CharacterMasterDetailPageDetail : ContentPage
+    {
+        public CharacterMasterDetailPageDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyXamarinAlliance/MyXamarinAlliance/Views/CharacterMasterDetailPageMaster.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinAllianceApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CharacterMasterDetailPageMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public CharacterMasterDetailPageMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new CharacterMasterDetailPageMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class CharacterMasterDetailPageMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<CharacterMasterDetailPageMenuItem> MenuItems { get; set; }
+
+            public CharacterMasterDetailPageMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<CharacterMasterDetailPageMenuItem>(new[]
+                {
+                    new CharacterMasterDetailPageMenuItem { Id = 0, Title = "Page 1" },
+                    new CharacterMasterDetailPageMenuItem { Id = 1, Title = "Page 2" },
+                    new CharacterMasterDetailPageMenuItem { Id = 2, Title = "Page 3" },
+                    new CharacterMasterDetailPageMenuItem { Id = 3, Title = "Page 4" },
+                    new CharacterMasterDetailPageMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
